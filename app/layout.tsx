e2c63c2812d0016c2904987fc0312f6dcb2796b9
alimvrai <alimvrai@gmail.com>
2026-03-06 23:17:34 +0000

    Add RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Debora ❤️ Alim",
+  description: "A special page for Debora",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
